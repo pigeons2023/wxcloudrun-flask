f77d8e94999a253beb1db302384c8aeec0318f8b
pigeons2023 <3481343411@qq.com>
2026-03-29 11:09:08 +0800

    1

--- config.py
+++ config.py
@@ -7,3 +7,7 @@ DEBUG = True
 username = os.environ.get("MYSQL_USERNAME", 'root')
 password = os.environ.get("MYSQL_PASSWORD", 'root')
 db_address = os.environ.get("MYSQL_ADDRESS", '127.0.0.1:3306')
+
+# 微信小程序配置
+APPID = os.environ.get("APPID", 'wxa015b0bacfabc252')
+APPSECRET = os.environ.get("APPSECRET", '338b44ffb4ba0b88131b06c7fcb7f06c')
--- wxcloudrun/views.py
+++ wxcloudrun/views.py
@@ -1,8 +1,11 @@
 from datetime import datetime
 from flask import render_template, request
+import requests
+import config
 from run import app
 from wxcloudrun.dao import delete_counterbyid, query_counterbyid, insert_counter, update_counterbyid
-from wxcloudrun.model import Counters
+from wxcloudrun.model import Counters, User
+from wxcloudrun import db
 from wxcloudrun.response import make_succ_empty_response, make_succ_response, make_err_response
 
 
@@ -64,3 +67,81 @@ def get_count():
     """
     counter = Counters.query.filter(Counters.id == 1).first()
     return make_succ_response(0) if counter is None else make_succ_response(counter.count)
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    """
+    :return: 登录结果
+    """
+    # 获取请求体参数
+    params = request.get_json()
+
+    # 检查参数
+    if 'name' not in params or 'student_id' not in params or 'code' not in params:
+        return make_err_response('缺少必要参数')
+
+    name = params['name']
+    student_id = params['student_id']
+    code = params['code']
+
+    # 调用微信API获取openid
+    appid = config.APPID
+    secret = config.APPSECRET
+    url = f"https://api.weixin.qq.com/sns/jscode2session?appid={appid}&secret={secret}&js_code={code}&grant_type=authorization_code"
+    response = requests.get(url)
+    data = response.json()
+
+    if 'errcode' in data:
+        return make_err_response(data['errmsg'])
+
+    openid = data['openid']
+
+    # 检查用户是否已存在
+    existing_user = User.query.filter(User.student_id == student_id).first()
+    if existing_user:
+        # 更新用户信息
+        existing_user.name = name
+        existing_user.openid = openid
+        existing_user.updated_at = datetime.now()
+        db.session.commit()
+        user_data = {
+            'id': existing_user.id,
+            'name': existing_user.name,
+            'student_id': existing_user.student_id,
+            'openid': existing_user.openid
+        }
+        return make_succ_response(user_data)
+    else:
+        # 创建新用户
+        new_user = User(
+            name=name,
+            student_id=student_id,
+            openid=openid
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        user_data = {
+            'id': new_user.id,
+            'name': new_user.name,
+            'student_id': new_user.student_id,
+            'openid': new_user.openid
+        }
+        return make_succ_response(user_data)
+
+
+@app.route('/api/user/<student_id>', methods=['GET'])
+def get_user(student_id):
+    """
+    :return: 用户信息
+    """
+    user = User.query.filter(User.student_id == student_id).first()
+    if not user:
+        return make_err_response('用户不存在')
+    user_data = {
+        'id': user.id,
+        'name': user.name,
+        'student_id': user.student_id,
+        'openid': user.openid
+    }
+    return make_succ_response(user_data)
